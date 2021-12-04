7e5c52e7d1a93808ce285964458ef99292c8921d
Selam Alem <selamtalem@gmail.com>
2021-12-04 16:23:39 -0600

    reated reducer and captured feeling input

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,13 +1,44 @@
+import {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 
 function Feeling() {
 
+    const [feeling, setFeeling] = useState(useSelector((store) => store.feedbackReducer.feeling));
+//onsole.log(feeling, 'feelingggg')
+    const handleNext = (event) => {
+        event.preventDefault();
+   console.log('feeling is', feeling)
+       
+    }
+ 
+
+
+
+
   
   
   return(
-      <div>
+<div>
+    <h2>How are you feeling today? </h2>
 
-      </div>
+    <form onSubmit={handleNext}>
+        <input 
+        required
+        type="number"
+        min="1"
+        max="5"
+     
+        value={feeling}
+        onChange={(event)=>setFeeling(event.target.value)}>
+        </input>
+
+        <button> NEXT</button>
+          
+    </form>
+
+
+</div>
   )
 }
   export default Feeling;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,49 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//importing hooks first
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import logger from 'redux-logger';
+
+
+
+const usersFeedback = [{ 
+    feeling: '',
+    understanding: '',
+    support: '',
+    comments: '',
+    support: '',
+
+}]
+
+const feedbackReducer = (state = usersFeedback, action) => {
+    if (action.type === 'SET_FEEDBACK') {
+      console.log(action);
+      return action.payload;
+    }
+    return state;
+  }
+
+
+
+
+  const reduxStore = createStore(
+    combineReducers({
+        feedbackReducer,
+    }),
+    applyMiddleware(logger)
+  );
+
+
+
+ReactDOM.render
+(<Provider store={reduxStore}>
+    <App />
+    </Provider>,
+     document.getElementById('root'));
+
 registerServiceWorker();
+
+
+
