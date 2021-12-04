08b5fb4ab911adeb332c546f417abd28b867c290
Selam Alem <selamtalem@gmail.com>
2021-12-04 14:11:02 -0600

    completed routes

---
+++ server/router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.router();
+const pool = require('../modules/pool');
+
+
+// Get all info from database
+router.get('/', (req, res) => {
+    let queryText = 'SELECT title, author FROM "feedback" ORDER BY "date" ';
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      console.log(results ,'this is the results')
+      res.send(result.rows); // request for rows
+    })
+    .catch(error => {
+      console.log('error getting info', error);
+      res.sendStatus(500);
+    });
+  });
+
+
+
+  // Adds a new book to the list of awesome reads
+// Request body must be a book object with a title and author.
+router.post('/',  (req, res) => {
+    let newFeedback = req.body; // this will have the value of our new feedback inserted by the user
+    console.log('this is the new feedback', newFeedback);
+  
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments" )
+                      VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support,newFeedback.comments])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log('Error adding new feedback', error);
+        res.sendStatus(500);
+      });
+  });
+
+
+  
+  module.exports = router; // exports to server
+
--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-
+const feedbackRouter = require('./routes/feedback.router.js')
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+
+app.use('/feedback', feedbackRouter); //http calls
+// '/feedback' is gonna route it to there which will go to route and get the info
 
 
 /** ---------- START SERVER ---------- **/
