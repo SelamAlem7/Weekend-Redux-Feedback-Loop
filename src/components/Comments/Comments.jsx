03eb2c70b0736a216c1f7cd16682a7bb09987513
Selam Alem <selamtalem@gmail.com>
2021-12-04 18:27:34 -0600

    comepleted comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,15 +1,55 @@
+import {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
 
 
 function Comments () {
 
+    
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [comments, setComments] = useState(useSelector((store) => store.feedbackReducer.comments));
+
+
+
+    const handleNext = (event) => {
+        event.preventDefault();
+   console.log('comments is', comments)
+        
+   dispatch({
+    type: 'SET_FEEDBACK',
+    payload: {
+        property: 'comments', //telling what property to grab
+        value: comments
+    }
+  })
+
+  history.push('/review')
   
+};
+ 
 
-return(
-    <div>
-<h4>comments</h4>
-    </div>
-)
+  return(
+        <div>
+            <h2>Any comments you want to leave ? </h2>
+
+                <form onSubmit={handleNext}>
+                    <input 
+                        type="text"
+                        value={comments}
+                        onChange={(event)=>setComments(event.target.value)}>
+                    </input>
+                    
+                        <button> NEXT</button>
+          
+                </form>
 
 
-}
+        </div>
+        )
+
+};
+
 export default Comments;
