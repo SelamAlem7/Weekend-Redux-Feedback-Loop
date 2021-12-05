d155495b9d4ba212341fec271bcf36e37f716648
Selam Alem <selamtalem@gmail.com>
2021-12-04 18:56:22 -0600

    rendered review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,25 @@
+import {useSelector} from 'react-redux';
+
+
 
 
 function Review () {
+
+    const review = useSelector ((store) => store.feedbackReducer);
+    console.log('Review is:', review);
+
+    
 
   
 
 return(
     <div>
+
+        <h3> Review your feedback: </h3>
+            <p> Feeling: {review.feeling} </p>
+            <p> Understanding: {review.understanding} </p>
+            <p> Support: {review.support} </p>
+            <p> Comments: {review.comments} </p>
 
     </div>
 )
