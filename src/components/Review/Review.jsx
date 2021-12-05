58933cc674db4d0660133e6d0fa64907e722445d
Selam Alem <selamtalem@gmail.com>
2021-12-04 18:56:36 -0600

    completed review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,26 +1,53 @@
 import {useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
+
 
 
 
 
 function Review () {
 
+    const history = useHistory();
     const review = useSelector ((store) => store.feedbackReducer);
     console.log('Review is:', review);
 
+
     
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: review
+        }).then(() => {
+            console.log('Sent POST');
+          
+        }).catch((error) => {
+            console.log('POST error form', error);
+            
+        })
+        
+  
+  }
 
   
 
 return(
     <div>
+        
 
         <h3> Review your feedback: </h3>
+        
+        <form onSubmit={handleSubmit}>
             <p> Feeling: {review.feeling} </p>
             <p> Understanding: {review.understanding} </p>
             <p> Support: {review.support} </p>
             <p> Comments: {review.comments} </p>
 
+            <button>Submit</button>
+
+        </form>
     </div>
 )
 
